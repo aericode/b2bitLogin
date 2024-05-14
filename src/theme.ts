2e7a68151535e6c320aa448cd073f0aab39db326
Eric Andrade <e.andrade5f@gmail.com>
2024-05-14 13:20:46 -0300

    chore: adds theme file

---
+++ src/theme.ts
@@ -0,0 +1,18 @@
+export const theme = {
+  COLORS:{
+    BLACK: "#000",
+    WHITE: "#FFF"
+  },
+  FONT_WEIGHT:{
+    REGULAR: 400,
+    MEDIUM: 500,
+    BOLD: 700
+  },
+  FONT_SIZE:{
+    SM:  '16px',
+    MD:  '18px',
+    LG:  '20px',
+    XL:  '25px',
+  }
+
+}
