52dc7a3efcb0c059926b39265ac948446e29fe23
Eric Andrade <e.andrade5f@gmail.com>
2024-05-14 13:40:54 -0300

    chore: updates theme file based on figma prototype

--- src/theme.ts
+++ src/theme.ts
@@ -1,18 +1,31 @@
 export const theme = {
   COLORS:{
-    BLACK: "#000",
-    WHITE: "#FFF"
+    //Primary
+    BLUE: "02274F",
+    YELLOW: "#FDCF00",
+
+    //Secondary
+    BLACK_2: "#262626",
+
+    WHITE: "#FFFFFF",
+    WHITE_1: "#FAFAFA",
+    WHITE_3: "#F1F1F1",
+
+    GRAY_6: "#999999"
+
   },
   FONT_WEIGHT:{
     REGULAR: 400,
     MEDIUM: 500,
+    SEMIBOLD: 600,
     BOLD: 700
   },
   FONT_SIZE:{
-    SM:  '16px',
-    MD:  '18px',
-    LG:  '20px',
-    XL:  '25px',
+    XSM: '12px',
+    SM:  '14px',
+    MD:  '16px',
+    LG:  '18px',
+    XL:  '23px',
   }
 
 }
