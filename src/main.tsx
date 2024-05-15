b81d524208b87f8126aed01ae3d00f864b779490
Eric Andrade <e.andrade5f@gmail.com>
2024-05-15 15:50:20 -0300

    feat: adds login card

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import ThemeContext from './contexts/ThemeContext/index.tsx'
+import { ThemeProvider } from 'styled-components'
+import { theme } from './theme.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeContext>
+    <ThemeProvider theme={theme}>
       <App />
-    </ThemeContext>
+    </ThemeProvider>
   </React.StrictMode>,
 )
