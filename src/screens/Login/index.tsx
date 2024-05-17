257fbe9acbacc9b6c57353d3bf36c566a7f6f724
Eric Andrade <e.andrade5f@gmail.com>
2024-05-17 16:55:25 -0300

    chore: moves login function to utils folder

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,39 +1,17 @@
-import { MouseEvent, useState } from 'react'
+import { useState } from 'react'
 import logo from '../../assets/B2Bit_logo.png'
 import { Container, HeaderImage, LoginCard, Wrapper } from './styles'
 import { InputBlock } from '../../components/InputBlock';
 import { Button } from '../../components/Button';
+import { handleSubmit } from '../../utils/handleSubmit';
 
-import axios from 'axios';
 
 
 export function Login() {
   const [emailInput, setEmailInput] = useState('');
   const [passwordInput, setPasswordInput] = useState('');
 
-  const handleSubmit = async () => {
-
-    try {
-      const payload = {
-        email: emailInput,
-        password: passwordInput,
-      };
-      const response = await axios.post(
-        'https://api.homologation.cliqdrive.com.br/auth/login/',
-        payload,
-        {
-          headers: {
-            'Accept': 'application/json;version=v1_web',
-            'Content-Type': 'application/json',
-          },
-        }
-      );
-
-      console.log('Response:', response.data);
-    } catch (error) {
-      if(error instanceof Error)console.error('Error:', error.message);
-    }
-  };
+  
 
   return (
     <Container>
@@ -58,7 +36,7 @@ export function Login() {
           />
 
           <Button
-            onClick={() => handleSubmit()}
+            onClick={() => handleSubmit(emailInput, passwordInput)}
             text='Sign In'
           />
         </Wrapper>
