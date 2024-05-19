bfef054abfe44a569ac3335dabe0dad72ac1a09a
Eric Andrade <e.andrade5f@gmail.com>
2024-05-19 14:44:10 -0300

    feat: adds logout function

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,27 @@
+import { useContext } from "react";
 import { Button } from "../Button";
 import { Container, Wrapper } from "./styles";
+import { UserContext } from "../../contexts/UserContext";
+import { UserData } from "../../Types/UserData";
+import { useNavigate } from "react-router-dom";
 
 export function Header() {
+  const navigate = useNavigate();
+  const { setUserData } = useContext(UserContext);
+
+  function logOut(){
+    setUserData!({} as UserData)
+    localStorage.setItem('accessToken', '');
+    localStorage.setItem('isLoggedIn', 'false');
+    navigate('/');
+  }
 
   return (
     <Container>
       <Wrapper>
         <Button
-          onClick={async () => { }}
-          text='Log out'
+          onClick={ () => logOut()}
+          text='Logout'
         />
       </Wrapper>
     </Container>
