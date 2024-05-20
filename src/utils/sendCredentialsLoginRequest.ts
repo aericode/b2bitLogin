ea029624b3e33530cc3c5927b9fd4b4bdc56846e
Eric Andrade <e.andrade5f@gmail.com>
2024-05-20 15:39:47 -0300

    feat: adds interceptor for credentials login function

--- src/utils/sendCredentialsLoginRequest.ts
+++ src/utils/sendCredentialsLoginRequest.ts
@@ -1,25 +1,41 @@
 import axios from "axios";
+import { AuthResponse } from "../Types/AuthResponse";
+
+const api = axios.create({
+  baseURL: 'https://api.homologation.cliqdrive.com.br',
+  headers: {
+    'Accept': 'application/json;version=v1_web',
+    'Content-Type': 'application/json',
+  },
+});
+
+api.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+api.interceptors.response.use(
+  (response) => {
+    return response.data;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export const sendCredentialsLoginRequest = async (emailInput: string, passwordInput: string) => {
-  
   try {
     const payload = {
       email: emailInput,
       password: passwordInput,
     };
-    const response = await axios.post(
-      'https://api.homologation.cliqdrive.com.br/auth/login/',
-      payload,
-      {
-        headers: {
-          'Accept': 'application/json;version=v1_web',
-          'Content-Type': 'application/json',
-        },
-      }
-    );
-
-    return response.data;
+    const response = await api.post('/auth/login/', payload) as AuthResponse;
+    return response;
   } catch (error) {
-    if(error instanceof Error)throw error;
+    throw error;
   }
 };
